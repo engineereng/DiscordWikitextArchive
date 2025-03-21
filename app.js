48c2bd9457ff3c0f5f793348b72a84058652feab
Collin Eng <eng.engineereng@gmail.com>
2025-03-20 22:49:58 -0700

    refactor: move message formatting and thread reading functions to a new archive.js file

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import {
 } from 'discord-interactions';
 import { getRandomEmoji, DiscordRequest } from './utils.js';
 import { promises as fs } from 'fs';
+import { formatMessageToWikitext, readDiscordThread } from './archive.js';
 
 // Create an express app
 const app = express();
@@ -23,90 +24,6 @@ const PORT = process.env.PORT || 3000;
 app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
   // Interaction type and data
   const { type, data, channel } = req.body;
-
-  /**
-   * Read a Discord thread
-   * @param {*} threadId The ID of the thread to read
-   * @returns A JSON array of messages from the thread
-   */
-  const readDiscordThread = async (threadId) => {
-    const response = await DiscordRequest(`channels/${threadId}/messages?limit=100`, {
-      method: 'GET'
-    });
-    const messages = await response.json();
-
-    // Log more details about each message
-    // messages.forEach(message => {
-    //   console.log('Message:', {
-    //     type: message.type,
-    //     content: message.content,
-    //     hasEmbeds: message.embeds?.length > 0,
-    //     hasAttachments: message.attachments?.length > 0,
-    //     timestamp: message.timestamp,
-    //     author: message.author.username,
-    //     message_reference: message.message_reference,
-    //     components: message.components,
-    //     flags: message.flags,
-    //     raw: message,
-    //     mentions: message.mentions,
-    //     mention_roles: message.mention_roles,
-    //     pinned: message.pinned,
-    //     mention_everyone: message.mention_everyone,
-    //     tts: message.tts,
-    //     position: message.position,
-    //     referenced_message: message.referenced_message
-    //   });
-    // });
-
-    return messages;
-  }
-
-  /**
-   * Format a message to wikitext
-   * @param {*} message The message to format. Format:
-   * @param {boolean} simpleDate Whether to use the simple date format (21:56) or the full date format (Fri, 21 Mar 2025 21:56)
-   * @returns A string of the message formatted as wikitext
-   */
-  const formatMessageToWikitext = (message, simpleDate = false) => {
-    // Wanted format:
-    // *21:56: [[User:Ironwestie|Ironwestie]]: Hello all.
-    // *21:56: [[User:Brunocoolgamers|Brunocoolgamers]]: hii
-    // *21:56: [[User:Pokemonfreak777|Pokemonfreak777]]: hello
-    const parts = [];
-    // Add timestamp and author
-    const timestamp = new Date(message.timestamp).toUTCString();
-    console.log(timestamp);
-    // timestamp is in format: Fri, 21 Mar 2025 21:56:00 GMT
-    // we want to format it to: 21:56
-    const timestampFormatted = simpleDate ? timestamp.slice(16, 22) : timestamp;
-
-    // TODO map the author to a wikitext link based on the username
-    parts.push(`*${timestampFormatted}: ${message.author.username}:`);
-
-    // Add text content if it exists
-    if (message.content) {
-      // TODO format content based on the content (see moot_compact.py)
-      parts.push(message.content);
-    }
-
-    // Add embed content if it exists
-    if (message.embeds?.length > 0) {
-      message.embeds.forEach(embed => {
-        if (embed.title) parts.push(`[Embed Title] ${embed.title}`);
-        if (embed.description) parts.push(`[Embed Description] ${embed.description}`);
-        if (embed.url) parts.push(`[Embed URL] ${embed.url}`);
-      });
-    }
-
-    // Add attachment URLs if they exist
-    if (message.attachments?.length > 0) {
-      message.attachments.forEach(attachment => {
-        parts.push(`[Attachment] ${attachment.url}`);
-      });
-    }
-
-    return parts.join(' ');
-  }
 
   const getAllowedRoles = async () => {
     const storedRoles = await fs.readFile('allowed_roles.json', 'utf8');
